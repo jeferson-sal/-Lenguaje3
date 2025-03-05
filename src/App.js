d7b60977fcb4731c81ccfeff9117181cfd35b4b3
bg14891 <bayronfranciscogodoy@gmail.com>
2025-03-04 22:08:32 -0600

    creacion de componentes, para que se abran en pestaña completa.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Navbar from './components/Navbar';
-
+import Testimonio from './components/Testimonio';
+import Portafolio from './components/Portafolio';
+import Nosotros from './components/Nosotros';
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
    <Router>
       <Navbar/>
     <Routes>
-      <Route path='/' exact Component={Home}/>
+      <Route path='/' element ={<Home/>}/>
+      <Route path='/Testimonio' element={<Testimonio/>}/>
+      <Route path='/Portafolio' element={<Portafolio/>}/>
+      <Route path='/Nosotros' element={<Nosotros/>}/>
     </Routes> 
  </Router>
     </>
---
+++ src/components/Testimonio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonio = () => {
+  return (
+    <div>
+            <h1>Testimonios</h1>
+            <p>Estos son algunos testimonios de nuestros clientes...</p>
+        </div>
+  )
+}
+
+export default Testimonio
