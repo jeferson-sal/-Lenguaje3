2ad7e02f4e4f8dbcc26a0b8ce81849d213d3b75a
Gabriela0613 <anyicanales@unah.hn>
2025-03-25 11:42:10 -0600

    avance de navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,17 @@ import Hamburguesa from './Hamburguesa.js';
 import { Link } from 'react-router-dom';
 import { MdDarkMode } from "react-icons/md";
 
-function Navbar() {    
+function Navbar() {
   const [clicked, setClicked] = useState(false);
+  const [isMonochrome, setIsMonochrome] = useState(false);
 
   const handleClick = () => {
     setClicked(!clicked);
+  };
+
+  const toggleMonochrome = () => {
+    setIsMonochrome(!isMonochrome);
+    document.body.classList.toggle('monochrome-mode');
   };
 
   const closeMenu = () => {
@@ -16,30 +22,31 @@ function Navbar() {
   };
 
   return (
-    <> 
-      <nav className='nav'> 
-        <img className='logo' src='../logo.jpg' alt='logo' />
-        <div className={`links ${clicked ? 'active' : ''}`}>
-          <Link className='alink' onClick={closeMenu} to='/'>Inicio</Link>
-          <Link className='alink' onClick={closeMenu} to='/Testimonio'>Testimonio</Link>
-          <Link className='alink' onClick={closeMenu} to='/Portafolio'>Portafolio</Link>
-          <Link className='alink' onClick={closeMenu} to='/Nosotros'>Nosotros</Link>
-        </div>
+    <nav className='nav'>
+      <img className='logo' src='../logo.jpg' alt='logo' />
 
-        {/* Contenedor de Login + Modo Oscuro */}
-        <div className="nav-right">
+      {/* Contenedor derecho con Login y Luna */}
+      <div className="right-section">
+        <button className="dark-mode-button" onClick={toggleMonochrome}>
           <MdDarkMode className="dark-mode-icon" />
-          <Link className="login-button" to='/login'>Login</Link>
-        </div>
+        </button>
+        <Link className='alink' onClick={closeMenu} to='/Login'>Login</Link>
+      </div>
 
-        <div className='ocultar'>  
-          <Hamburguesa clicked={clicked} handleClick={handleClick} />
-        </div>
+      {/* Contenedor de los enlaces principales */}
+      <div className={`links ${clicked ? 'active' : ''}`}>
+        <Link className='alink' onClick={closeMenu} to='/'>Inicio</Link>
+        <Link className='alink' onClick={closeMenu} to='/Testimonio'>Testimonio</Link>
+        <Link className='alink' onClick={closeMenu} to='/Portafolio'>Portafolio</Link>
+        <Link className='alink' onClick={closeMenu} to='/Nosotros'>Nosotros</Link>
+      </div>
 
-        <div className={`initial ${clicked ? 'active' : ''}`}></div>
-      </nav>   
-    </> 
+      <div className='ocultar'>
+        <Hamburguesa clicked={clicked} handleClick={handleClick} />
+      </div>
+    </nav>
   );
 }
 
 export default Navbar;
+
